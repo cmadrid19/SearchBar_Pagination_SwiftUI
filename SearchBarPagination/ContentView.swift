7423da222bc71e2d698d5324dd2dfab97fa9be11
Max <maxim.macari.dam@gmail.com>
2020-10-17 19:54:22 +0200

    Initial Commit

---
+++ SearchBarPagination/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SearchBarPagination
+//
+//  Created by Maxim Macari on 17/10/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SearchBarPagination/SearchBarPaginationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchBarPaginationApp.swift
+//  SearchBarPagination
+//
+//  Created by Maxim Macari on 17/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct SearchBarPaginationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
